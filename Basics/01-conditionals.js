048e94418fa7a00d6220bb3ba0e7ad50f2710683
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-01-22 23:42:16 +0530

    conditions

---
+++ Basics/01-conditionals.js
@@ -0,0 +1,34 @@
+// var authenticated = true;
+
+// // if(authenticated){
+// //     console.log("Signned in")
+// // } else {
+// //     console.log("please sign in")
+// // }
+
+// authenticated ? console.log('SignedIn') : console.log('Please SignIn');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
