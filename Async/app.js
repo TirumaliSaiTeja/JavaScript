26d46d9fc17469aeee1d5a2c8645f8b04ae7f51e
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-02-04 22:53:12 +0530

    sync and async

---
+++ Async/app.js
@@ -0,0 +1,25 @@
+// //sync function
+
+// function myfunc () {
+//     console.log('Welcome')
+// }
+
+// console.log("start");
+
+// myfunc();
+
+
+// console.log("end");
+
+// Async function
+
+// console.log("start");
+
+// the settimeout will be pass on to the web api and it will handle
+// all the function and pushed back to callstack
+
+// setTimeout(()=>{
+//     console.log("welcome");
+// }, 8000);
+
+// console.log("end");
