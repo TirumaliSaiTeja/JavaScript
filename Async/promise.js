b7717a01bc35435e6d0387445c0e5f9418459e13
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-02-04 23:22:44 +0530

    using promises

---
+++ Async/promise.js
@@ -0,0 +1,11 @@
+const promise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log('new user');
+        reject(new Error('user not logged in'));
+    },2000);
+});
+
+
+promise.then(user=>{
+    console.log(user);
+}). catch(err=>console.log(err));
