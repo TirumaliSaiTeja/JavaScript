4f19d3e584a311f19c2a2db88c5d716b95ed6c95
Sai Teja <54593666+TirumaliSaiTeja@users.noreply.github.com>
2021-05-11 23:09:30 +0530

    Update 01-conditionals.js

--- Basics/01-conditionals.js
+++ Basics/01-conditionals.js
@@ -31,6 +31,12 @@ The switch statement is used to perform different actions based on different con
 
 The For/In Loop
 The JavaScript for/in statement loops through the properties of an Object
+  
+  
+ The For/Of Loop
+The JavaScript for/of statement loops through the values of an iterable object.
+
+It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more 
 
 
 
