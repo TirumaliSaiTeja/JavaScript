212edd84151f1bba68cf79999850d2b24403b7aa
Sai Teja <54593666+TirumaliSaiTeja@users.noreply.github.com>
2021-05-11 23:11:03 +0530

    Update 01-conditionals.js

--- Basics/01-conditionals.js
+++ Basics/01-conditionals.js
@@ -44,6 +44,10 @@ The while loop loops through a block of code as long as a specified condition is
 
 
 
+The break statement "jumps out" of a loop.
+
+The continue statement "jumps over" one iteration in the loop.
+
 
 
 
