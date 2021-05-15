6efa05ee1a5938180ea657e79428f201088cf01a
Sai Teja <54593666+TirumaliSaiTeja@users.noreply.github.com>
2021-05-15 23:25:55 +0530

    Update 01-conditionals.js

--- Basics/01-conditionals.js
+++ Basics/01-conditionals.js
@@ -17,7 +17,7 @@ You can use conditional statements in your code to do this.
 
 In JavaScript we have the following conditional statements:
 
-Use if to specify a block of code to be executed, if a specified condition is true
+Use if to specify a block of code to be executed, if a specified condition is of true value
 Use else to specify a block of code to be executed, if the same condition is false
 Use else if to specify a new condition to test, if the first condition is false
 Use switch to specify many alternative blocks of code to be executed
