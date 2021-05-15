18607e35d3f07a443e8838946249a2c49517aa94
Sai Teja <54593666+TirumaliSaiTeja@users.noreply.github.com>
2021-05-15 23:26:42 +0530

    Update 01-conditionals.js

--- Basics/01-conditionals.js
+++ Basics/01-conditionals.js
@@ -36,7 +36,7 @@ The JavaScript for/in statement loops through the properties of an Object
  The For/Of Loop
 The JavaScript for/of statement loops through the values of an iterable object.
 
-It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more 
+It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists
 
 
 
