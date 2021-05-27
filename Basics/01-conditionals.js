76cf2b23f1c5e9cd1d9de1d50c3aae4b486da39a
Sai Teja <54593666+TirumaliSaiTeja@users.noreply.github.com>
2021-05-27 23:07:19 +0530

    Update 01-conditionals.js

--- Basics/01-conditionals.js
+++ Basics/01-conditionals.js
@@ -1,12 +1,4 @@
-// var authenticated = true;
-
-// // if(authenticated){
-// //     console.log("Signned in")
-// // } else {
-// //     console.log("please sign in")
-// // }
-
-// authenticated ? console.log('SignedIn') : console.log('Please SignIn');
+// 
 
 
 
